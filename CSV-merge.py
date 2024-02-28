046c49310583ed9a481dc4c4c0ed7effe400c810
BradEdgeworth <161642184+BradEdgeworth@users.noreply.github.com>
2024-02-28 14:22:03 -0500

    Add files via upload

---
+++ CSV-merge.py
@@ -0,0 +1,63 @@
+#  Script to query all the CSV files in a directory that might have different columns and merge into a single CSV file.
+#  This does not take into account duplicate rows; they will still be processed.
+#  By Brad Edgeworth
+#
+#  Version 2024-02-28
+#
+#  Python dependencies:
+#     pandas  (i.e. pip install pandas
+
+
+
+
+
+from csv import DictReader
+import glob
+import re
+import pandas as pd
+from datetime import datetime
+finalcsv_column_names = ['']
+mainData = []
+regex = '!Consolidate*'
+today = datetime.today()
+d4 = datetime.now().strftime("%Y-%m-%d---%H-%M-%S")
+ 
+# list all csv files only that do not start with Consolidate
+csv_files = glob.glob('[!Consolidate]*.{}'.format('csv'))
+print(csv_files)
+print(' ------------' )
+
+for file in csv_files:
+   df = pd.read_csv(file)
+   currentcsv_column_names = list(df.columns)
+   #print ('List of column names: ', currentcsv_column_names)
+   #print (currentcsv_column_names)
+   finalcsv_column_names = finalcsv_column_names + currentcsv_column_names
+  
+finalcsv_column_names = list(set(finalcsv_column_names))
+finalcsv_column_names.sort()
+
+# --------------------------------------------------------------------------------
+# Unremark following line and use for old Device Templates
+# finalcsv_column_names = ['csv-deviceId','csv-deviceIP'] + finalcsv_column_names
+# --------------------------------------------------------------------------------
+#  Use for new ConfigurationGroups.   Remark if using the old Device Templates
+finalcsv_column_names = ['Device ID','Host Name'] + finalcsv_column_names
+# print ('Final list of column Headers: ', finalcsv_column_names)
+
+for file in csv_files:
+  with open(file, 'r', encoding='utf-8') as read_obj:
+        csv_dict_reader = DictReader(read_obj)
+        for row in csv_dict_reader:
+            companyRowData = []
+            for headerName in finalcsv_column_names:
+                try:
+                    companyRowData.append(row[headerName])
+                except:
+                    companyRowData.append(' ')
+            mainData.append(companyRowData)
+        print(" Processed File : " + str(file))
+print("------------------------------------------------------")
+companyData = pd.DataFrame(mainData, columns=finalcsv_column_names)
+newConsolidatedCSVfilename = 'ConsolidatedCSV-'+d4+'.csv'
+companyData.to_csv(newConsolidatedCSVfilename, index=False)
---
+++ README.txt
@@ -0,0 +1,18 @@
+# Overview
+A python script to read a set of CSV files (that the script is launched from), read all of the column headers, and put into a master column header list.   Then it will consolidate allof the other rows from the CSV files, and insert into the appropriate rows. 
+
+This tool was created to assist with Configuration CSVs. 
+
+
+# Use
+
+Running CSV-Merge.py queries local directory for all CSVs (except those that start with consolidate)  
+
+# Install
+Requires python 3.9 or later
+
+Install dependencies:
+    pip install pandas
+
+# Author
+Brad Edgeworth, brad@config-t.com
