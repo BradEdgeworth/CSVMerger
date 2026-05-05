c23d033509b9d0ea35b24400f075ac64558f1552
BradEdgeworth <161642184+BradEdgeworth@users.noreply.github.com>
2026-05-05 14:45:22 -0400

    Update CSV-merge-DeviceTemplate.py

--- CSV-merge-DeviceTemplate.py
+++ CSV-merge-DeviceTemplate.py
@@ -2,14 +2,19 @@
 #  This does not take into account duplicate rows; they will still be processed.
 #  By Brad Edgeworth
 #
+#  Version 2024-10-24
+#     Fixed an issue where not all CSV files were returned
+#
 #  Version 2024-03-06
 #      Fixed a bug with CSV format inserting an unneccessary space ' '
 #
+#  Version 2026-04-28
+#      Fixed a bug where a blank column and a duplciate deviceID is present. 
+#
 #  Python dependencies:
-#     pandas  (i.e. pip install pandas
-
-
-
+#     pandas  (i.e. pip install pandas )
+print('CSV consolidation tool.   Version: 2026-04-28')
+print('---------------------------------------------')
 
 
 from csv import DictReader
@@ -25,28 +30,37 @@ today = datetime.today()
 d4 = datetime.now().strftime("%Y-%m-%d---%H-%M-%S")
  
 # list all csv files only that do not start with Consolidate
-csv_files = glob.glob('[!Consolidate]*.{}'.format('csv'))
+all_csv_files = glob.glob("*.csv")
+
+# Exclude files starting with 'ConsolidatedCSV'
+csv_files = [f for f in all_csv_files if not f.startswith("ConsolidatedCSV")]
 print('These are the CSV files that are being consolidated:')
 print('------------------------------------------------------')
-
+for f in csv_files:
+    print(f)
+    
+currentcsv_column_names = set ()
 for file in csv_files:
    df = pd.read_csv(file)
-   currentcsv_column_names = list(df.columns)
+   currentcsv_column_names.update(df.columns)
    #print ('List of column names: ', currentcsv_column_names)
    #print (currentcsv_column_names)
-   finalcsv_column_names = finalcsv_column_names + currentcsv_column_names
-  
-finalcsv_column_names = list(set(finalcsv_column_names))
-finalcsv_column_names.sort()
-
-# --------------------------------------------------------------------------------
-# Unremark following line and use for old Device Templates
-finalcsv_column_names = ['csv-deviceId','csv-deviceIP'] + finalcsv_column_names
 
 # --------------------------------------------------------------------------------
 #  Use for new ConfigurationGroups.   Remark if using the old Device Templates
-#  finalcsv_column_names = ['Device ID','Host Name'] + finalcsv_column_names
-# print ('Final list of column Headers: ', finalcsv_column_names)
+#primary_headers = ['Device ID','Host Name']
+
+# --------------------------------------------------------------------------------
+# Unremark following line and use for old Device Templates
+primary_headers = ['csv-deviceId', 'csv-deviceIP', 'csv-host-name']
+  
+for h in primary_headers:
+    currentcsv_column_names.discard(h)
+currentcsv_column_names.discard('')
+  
+finalcsv_column_names = primary_headers + sorted(list(currentcsv_column_names))
+
+
 
 for file in csv_files:
   with open(file, 'r', encoding='utf-8') as read_obj:
